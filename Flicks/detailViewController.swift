0e98b1d118ec08902164b798ebc8cb36e99ac016
Aniema Ubom <a.a.ubom@gmail.com>
2017-02-07 19:28:58 -0500

    added navigation + added detail view and view controller and uncommented prepare for segue

---
+++ Flicks/detailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  detailViewController.swift
+//  Flicks
+//
+//  Created by A on 2/7/17.
+//  Copyright © 2017 samman. All rights reserved.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var overviewLabelView: UILabel!
+    @IBOutlet weak var titleLabelView: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -120,14 +120,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         task.resume()
     }
     
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        print("Prepare for segue")
     }
-    */
 
 }
